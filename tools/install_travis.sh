a58f13970164ce2fc1b70289338ae7e68f7a09ea
Francesco Biscani <bluescarni@gmail.com>
2016-09-10 16:12:04 +0200

    First travis attempt.

---
+++ tools/install_travis.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+if [[ "${BUILD_TYPE}" == "Debug" ]]; then
+    if [[ "${MPPP_COMPILER}" == "gcc" ]]; then
+        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
+        make;
+        ctest -V;
+    elif [[ "${MPPP_COMPILER}" == "clang" ]]; then
+        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE ../;
+        make;
+        ctest -V;
+    fi
+elif [[ "${BUILD_TYPE}" == "Coverage" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE -DCMAKE_CXX_FLAGS="--coverage" ../;
+    make;
+    ctest -V;
+elif [[ "${BUILD_TYPE}" == "Release" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE ../;
+    make;
+    ctest -V;
+fi
