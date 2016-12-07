0ab5962b2afd0a21817062024307da4a965d6c3c
Francesco Biscani <bluescarni@gmail.com>
2016-12-08 00:35:27 +0100

    Enable GCC 6 and more sanitizer builds.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -5,16 +5,16 @@ set -e
 # Echo each command
 set -x
 
-if [[ "${BUILD_TYPE}" == "Debug" ]]; then
-    if [[ "${MPPP_COMPILER}" == "gcc" ]]; then
-        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
-        make;
-        ctest -V;
-    elif [[ "${MPPP_COMPILER}" == "clang" ]]; then
-        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-        make;
-        ctest -V;
-    fi
+if [[ "${BUILD_TYPE}" == "Debug"]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
+    make;
+    ctest -V;
+elif [[ "${BUILD_TYPE}" == "DebugAS" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
+elif [[ "${BUILD_TYPE}" == "DebugTS" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=thread" ../;
+elif [[ "${BUILD_TYPE}" == "DebugUBS" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=undefined" ../;
 elif [[ "${BUILD_TYPE}" == "Coverage" ]]; then
     cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-Og --coverage" ../;
     make;
