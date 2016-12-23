3da17965795b6faa884e89388139f9ac718a5099
Francesco Biscani <bluescarni@gmail.com>
2016-12-23 11:28:31 +0100

    More debug.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -5,24 +5,24 @@ set -e
 # Echo each command
 set -x
 
-if [[ "${BUILD_TYPE}" == "ReleaseGCC48" ]]; then
+if [[ "${MPPP_BUILD}" == "ReleaseGCC48" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
     make -j2;
     ctest -V;
-elif [[ "${BUILD_TYPE}" == "DebugGCC48" ]]; then
+elif [[ "${MPPP_BUILD}" == "DebugGCC48" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
     make -j2;
     ctest -V;
-elif [[ "${BUILD_TYPE}" == "CoverageGCC5" ]]; then
+elif [[ "${MPPP_BUILD}" == "CoverageGCC5" ]]; then
     CXX=g++-5 CC=gcc-5 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="--coverage" ../;
     make -j2;
     ctest -V;
     bash <(curl -s https://codecov.io/bash) -x gcov-5
-elif [[ "${BUILD_TYPE}" == "DebugClang39" ]]; then
+elif [[ "${MPPP_BUILD}" == "DebugClang39" ]]; then
     CXX=clang++-3.9 CC=clang-3.9 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
     make -j2;
     ctest -V;
-elif [[ "${BUILD_TYPE}" == "ReleaseClang39" ]]; then
+elif [[ "${MPPP_BUILD}" == "ReleaseClang39" ]]; then
     CXX=clang++-3.9 CC=clang-3.9 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
     make -j2;
     ctest -V;
