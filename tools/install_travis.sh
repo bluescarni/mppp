d513d44cf4c9fed03482c2b9acc8918199e6bb3c
Francesco Biscani <bluescarni@gmail.com>
2016-12-23 11:14:41 +0100

    Attempt to sanitize travis a bit.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -5,23 +5,25 @@ set -e
 # Echo each command
 set -x
 
-if [[ "${BUILD_TYPE}" == "Debug" ]]; then
-    if [[ "${MPPP_COMPILER}" == "gcc" ]]; then
-        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
-        make;
-        ctest -V;
-    elif [[ "${MPPP_COMPILER}" == "clang" ]]; then
-        cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-        make;
-        ctest -V;
-    fi
-elif [[ "${BUILD_TYPE}" == "Coverage" ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="--coverage" ../;
-    make;
+if [[ "${BUILD_TYPE}" == "ReleaseGCC48" ]]; then
+    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
+    make -j2;
     ctest -V;
-    bash <(curl -s https://codecov.io/bash) -x $GCOV_EXECUTABLE
-elif [[ "${BUILD_TYPE}" == "Release" ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-    make;
+elif [[ "${BUILD_TYPE}" == "DebugGCC48" ]]; then
+    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
+    make -j2;
+    ctest -V;
+elif [[ "${BUILD_TYPE}" == "CoverageGCC5" ]]; then
+    CXX=g++-5 CC=gcc-5 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="--coverage" ../;
+    make -j2;
+    ctest -V;
+    bash <(curl -s https://codecov.io/bash) -x gcov-5
+elif [[ "${BUILD_TYPE}" == "DebugClang39" ]]; then
+    CXX=clang++-3.9 CC=clang-3.9 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
+    make -j2;
+    ctest -V;
+elif [[ "${BUILD_TYPE}" == "ReleaseClang39" ]]; then
+    CXX=clang++-3.9 CC=clang-3.9 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
+    make -j2;
     ctest -V;
 fi
