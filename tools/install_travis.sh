d68b07b0f8123b46965c0a7f382d20b7c3fc25f8
Francesco Biscani <bluescarni@gmail.com>
2016-12-23 13:41:10 +0100

    travis: attempt 32-bit coverage build with GCC 6.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -21,7 +21,7 @@ elif [[ "${MPPP_BUILD}" == "DebugGCC48DebugGMP" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 ./configure --disable-shared --enable-assert --enable-alloca=debug --disable-assembly CFLAGS=-g;
     make -j2;
     cd ..;
-    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DGMP_INCLUDE_DIR=$TRAVIS_BUILD_DIR/build/gmp-6.1.2 ../ -DGMP_LIBRARY=$TRAVIS_BUILD_DIR/build/gmp-6.1.2/.libs/libgmp.a;
+    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DGMP_INCLUDE_DIR=$TRAVIS_BUILD_DIR/build/gmp-6.1.2 -DGMP_LIBRARY=$TRAVIS_BUILD_DIR/build/gmp-6.1.2/.libs/libgmp.a ../;
     make -j2 VERBOSE=1;
     ctest -V;
 elif [[ "${MPPP_BUILD}" == "CoverageGCC5" ]]; then
@@ -29,6 +29,17 @@ elif [[ "${MPPP_BUILD}" == "CoverageGCC5" ]]; then
     make -j2 VERBOSE=1;
     ctest -V;
     bash <(curl -s https://codecov.io/bash) -x gcov-5
+elif [[ "${MPPP_BUILD}" == "Coverage32GCC6" ]]; then
+    wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2;
+    tar xjvf gmp-6.1.2.tar.bz2;
+    cd gmp-6.1.2;
+    CXX=g++-6 CC=gcc-6 ABI=32 ./configure --disable-shared;
+    make -j2;
+    cd ..;
+    CXX=g++-6 CC=gcc-6 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-m32 --coverage" -DGMP_INCLUDE_DIR=$TRAVIS_BUILD_DIR/build/gmp-6.1.2 -DGMP_LIBRARY=$TRAVIS_BUILD_DIR/build/gmp-6.1.2/.libs/libgmp.a ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+    bash <(curl -s https://codecov.io/bash) -x gcov-6
 elif [[ "${MPPP_BUILD}" == "DebugClang38" ]]; then
     CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
     make -j2 VERBOSE=1;
