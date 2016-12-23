37ed16777decea8cbe3454cf1fd28920427153d4
Francesco Biscani <bluescarni@gmail.com>
2016-12-23 12:33:21 +0100

    Verbose compilation.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -7,23 +7,23 @@ set -x
 
 if [[ "${MPPP_BUILD}" == "ReleaseGCC48" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-    make -j2;
+    make -j2 VERBOSE=1;
     ctest -V;
 elif [[ "${MPPP_BUILD}" == "DebugGCC48" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" ../;
-    make -j2;
+    make -j2 VERBOSE=1;
     ctest -V;
 elif [[ "${MPPP_BUILD}" == "CoverageGCC5" ]]; then
     CXX=g++-5 CC=gcc-5 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes -DCMAKE_CXX_FLAGS="--coverage" ../;
-    make -j2;
+    make -j2 VERBOSE=1;
     ctest -V;
     bash <(curl -s https://codecov.io/bash) -x gcov-5
 elif [[ "${MPPP_BUILD}" == "DebugClang38" ]]; then
     CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-    make -j2;
+    make -j2 VERBOSE=1;
     ctest -V;
 elif [[ "${MPPP_BUILD}" == "ReleaseClang38" ]]; then
     CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=yes -DMPPP_WITH_LONG_DOUBLE=yes ../;
-    make -j2;
+    make -j2 VERBOSE=1;
     ctest -V;
 fi
