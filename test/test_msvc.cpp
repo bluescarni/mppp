915760ef6cfd3e70576cc8b61a31e1bd9decbbc8
Francesco Biscani <bluescarni@gmail.com>
2016-12-29 00:02:10 +0100

    msvc debugging.

---
+++ test/test_msvc.cpp
@@ -0,0 +1,46 @@
+/* Copyright 2009-2016 Francesco Biscani (bluescarni@gmail.com)
+
+This file is part of the mp++ library.
+
+The mp++ library is free software; you can redistribute it and/or modify
+it under the terms of either:
+
+  * the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+or
+
+  * the GNU General Public License as published by the Free Software
+    Foundation; either version 3 of the License, or (at your option) any
+    later version.
+
+or both in parallel, as here.
+
+The mp++ library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the mp++ library.  If not,
+see https://www.gnu.org/licenses/. */
+
+#include <iostream>
+
+#include <mp++.hpp>
+
+#include "test_utils.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+using namespace mppp;
+using namespace mppp::mppp_impl;
+using namespace mppp_test;
+
+TEST_CASE("msvc")
+{
+    using integer = mp_integer<1>;
+    std::cout << integer{} << '\n';
+}
