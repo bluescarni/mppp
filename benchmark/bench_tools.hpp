93f84a406ebf1c34f39db168e15c8471e614c33e
Francesco Biscani <bluescarni@gmail.com>
2017-01-05 18:10:24 +0100

    More benchmark work.

--- benchmark/bench_tools.hpp
+++ benchmark/bench_tools.hpp
@@ -34,12 +34,19 @@ see https://www.gnu.org/licenses/. */
 #include <chrono>
 #include <cstdlib>
 #include <fstream>
+#include <gmp.h>
 #include <limits>
+#include <mp++.hpp>
 #include <random>
+#include <sstream>
 #include <string>
 #include <type_traits>
 #include <utility>
 #include <vector>
+
+#if defined(MPPP_BENCHMARK_FLINT)
+#include <flint/fmpz.h>
+#endif
 
 namespace mppp_bench
 {
@@ -96,7 +103,7 @@ inline void benchmark_suite(const std::string &name, Args &&... args)
          "    mpl.use('Agg')\n"
          "    import seaborn as sns\n"
          "    df = get_data()\n"
-         "    g = sns.barplot(data=df,palette='muted')\n"
+         "    g = sns.barplot(data=df)\n"
          "    for l in g.get_xticklabels():\n"
          "        l.set_rotation(45)\n"
          "    g.set_title('"
@@ -168,6 +175,187 @@ struct bench_conv {
     }
     std::vector<Integer> m_randoms;
 };
+
+inline namespace impl
+{
+
+template <typename T, typename std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value, int>::type = 0>
+inline long long lex_cast_tr(T n)
+{
+    return static_cast<long long>(n);
+}
+
+template <typename T, typename std::enable_if<std::is_integral<T>::value && std::is_unsigned<T>::value, int>::type = 0>
+inline unsigned long long lex_cast_tr(T n)
+{
+    return static_cast<unsigned long long>(n);
+}
+
+template <typename T, typename std::enable_if<!std::is_integral<T>::value, int>::type = 0>
+inline const T &lex_cast_tr(const T &x)
+{
+    return x;
+}
+}
+
+// Lexical cast: retrieve the string representation of input object x.
+template <typename T>
+inline std::string lex_cast(const T &x)
+{
+    std::ostringstream oss;
+    oss << lex_cast_tr(x);
+    return oss.str();
+}
+
+inline std::string lex_cast(const mppp::mppp_impl::mpz_raii &m)
+{
+    return mppp::mppp_impl::mpz_to_str(&m.m_mpz);
+}
+
+// Set mpz to random value with n limbs, with the most significant limb divided optionally by div.
+inline void random_mpz(mppp::mppp_impl::mpz_raii &m, unsigned n, std::mt19937 &rng, ::mp_limb_t div = 1u)
+{
+    if (!n) {
+        ::mpz_set_ui(&m.m_mpz, 0);
+        return;
+    }
+    static thread_local mppp::mppp_impl::mpz_raii tmp;
+    std::uniform_int_distribution<::mp_limb_t> dist(0u, std::numeric_limits<::mp_limb_t>::max());
+    // Set the first limb.
+    ::mpz_set_str(&m.m_mpz, lex_cast((dist(rng) & GMP_NUMB_MASK) / div).c_str(), 10);
+    for (unsigned i = 1u; i < n; ++i) {
+        ::mpz_set_str(&tmp.m_mpz, lex_cast(dist(rng) & GMP_NUMB_MASK).c_str(), 10);
+        ::mpz_mul_2exp(&m.m_mpz, &m.m_mpz, GMP_NUMB_BITS);
+        ::mpz_add(&m.m_mpz, &m.m_mpz, &tmp.m_mpz);
+    }
+}
+
+// Set integer to random value with n limbs, with the most significant limb divided optionally by div.
+template <typename Integer>
+inline void random_integer(Integer &m, unsigned n, std::mt19937 &rng, ::mp_limb_t div = 1u)
+{
+    static thread_local mppp::mppp_impl::mpz_raii tmp;
+    random_mpz(tmp, n, rng, div);
+    m = Integer(lex_cast(tmp));
+}
+
+#define MPPP_BENCHMARK_VEC_SIZE 10000u
+
+// mp++ benchmarking.
+template <typename Integer>
+struct uadd_vec_mppp {
+    static constexpr unsigned size = MPPP_BENCHMARK_VEC_SIZE;
+    uadd_vec_mppp(std::mt19937 &rng, unsigned N, unsigned M)
+    {
+        std::for_each(m_arr1.begin(), m_arr1.end(), [&rng, N](Integer &i) { random_integer(i, N, rng, 2); });
+        std::for_each(m_arr2.begin(), m_arr2.end(), [&rng, M](Integer &i) { random_integer(i, M, rng, 2); });
+    }
+    void operator()() const
+    {
+        for (auto j = 0u; j < size; ++j) {
+            add(m_arr3[j], m_arr1[j], m_arr2[j]);
+        }
+    }
+    std::array<Integer, size> m_arr1;
+    std::array<Integer, size> m_arr2;
+    mutable std::array<Integer, size> m_arr3;
+};
+
+// GMP benchmarking.
+struct uadd_vec_gmp {
+    static constexpr unsigned size = MPPP_BENCHMARK_VEC_SIZE;
+    uadd_vec_gmp(std::mt19937 &rng, unsigned N, unsigned M)
+    {
+        std::for_each(m_arr1.begin(), m_arr1.end(),
+                      [&rng, N](mppp::mppp_impl::mpz_raii &m) { random_mpz(m, N, rng, 2); });
+        std::for_each(m_arr2.begin(), m_arr2.end(),
+                      [&rng, M](mppp::mppp_impl::mpz_raii &m) { random_mpz(m, M, rng, 2); });
+    }
+    void operator()() const
+    {
+        for (auto j = 0u; j < size; ++j) {
+            ::mpz_add(&m_arr3[j].m_mpz, &m_arr1[j].m_mpz, &m_arr2[j].m_mpz);
+        }
+    }
+    std::array<mppp::mppp_impl::mpz_raii, size> m_arr1;
+    std::array<mppp::mppp_impl::mpz_raii, size> m_arr2;
+    mutable std::array<mppp::mppp_impl::mpz_raii, size> m_arr3;
+};
+
+#if defined(MPPP_BENCHMARK_FLINT)
+
+// Flint benchmarking.
+struct fmpz_raii {
+    fmpz_raii()
+    {
+        ::fmpz_init(m_fmpz);
+    }
+    ~fmpz_raii()
+    {
+        ::fmpz_clear(m_fmpz);
+    }
+    ::fmpz_t m_fmpz;
+};
+
+struct uadd_vec_fmpz {
+    static constexpr unsigned size = MPPP_BENCHMARK_VEC_SIZE;
+    uadd_vec_fmpz(std::mt19937 &rng, unsigned N, unsigned M)
+    {
+        mppp::mppp_impl::mpz_raii m;
+        std::for_each(m_arr1.begin(), m_arr1.end(), [&rng, N, &m](fmpz_raii &f) {
+            random_mpz(m, N, rng, 8);
+            ::fmpz_set_str(f.m_fmpz, lex_cast(m).c_str(), 10);
+        });
+        std::for_each(m_arr2.begin(), m_arr2.end(), [&rng, M, &m](fmpz_raii &f) {
+            random_mpz(m, M, rng, 8);
+            ::fmpz_set_str(f.m_fmpz, lex_cast(m).c_str(), 10);
+        });
+    }
+    void operator()() const
+    {
+        for (auto j = 0u; j < size; ++j) {
+            ::fmpz_add(m_arr3[j].m_fmpz, m_arr1[j].m_fmpz, m_arr2[j].m_fmpz);
+        }
+    }
+    std::array<fmpz_raii, size> m_arr1;
+    std::array<fmpz_raii, size> m_arr2;
+    mutable std::array<fmpz_raii, size> m_arr3;
+};
+
+#endif
+
+#if defined(MPPP_BENCHMARK_BOOST)
+
+// Boost benchmarking.
+template <typename Integer>
+struct uadd_vec_boost {
+    static constexpr unsigned size = MPPP_BENCHMARK_VEC_SIZE;
+    uadd_vec_boost(std::mt19937 &rng, unsigned N, unsigned M)
+    {
+        mppp::mppp_impl::mpz_raii m;
+        std::for_each(m_arr1.begin(), m_arr1.end(), [&rng, N, &m](Integer &n) {
+            random_mpz(m, N, rng, 8);
+            n = Integer(lex_cast(m));
+        });
+        std::for_each(m_arr2.begin(), m_arr2.end(), [&rng, M, &m](Integer &n) {
+            random_mpz(m, M, rng, 8);
+            n = Integer(lex_cast(m));
+        });
+    }
+    void operator()() const
+    {
+        for (auto j = 0u; j < size; ++j) {
+            m_arr3[j] = m_arr1[j] + m_arr2[j];
+        }
+    }
+    std::array<Integer, size> m_arr1;
+    std::array<Integer, size> m_arr2;
+    mutable std::array<Integer, size> m_arr3;
+};
+
+#endif
+
+#undef MPPP_BENCHMARK_VEC_SIZE
 }
 
 #endif
