e6fd1caf80a8670eabc971f6dd3840fcda75f718
Francesco Biscani <bluescarni@gmail.com>
2017-05-14 15:30:55 +0200

    Add header with fwd declarations in detail.

---
+++ include/mp++/detail/fwd_decl.hpp
@@ -0,0 +1,24 @@
+// Copyright 2016-2017 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the mp++ library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef MPPP_DETAIL_FWD_DECL_HPP
+#define MPPP_DETAIL_FWD_DECL_HPP
+
+#include <cstddef>
+
+namespace mppp
+{
+
+template <std::size_t>
+class integer;
+
+template <std::size_t>
+class rational;
+}
+
+#endif
