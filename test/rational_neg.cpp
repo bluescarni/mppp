a995a8fdc7c5bd1742415edf549a00d2262ea979
Francesco Biscani <bluescarni@gmail.com>
2017-06-07 22:54:07 +0200

    Add neg testing.

---
+++ test/rational_neg.cpp
@@ -0,0 +1,92 @@
+// Copyright 2016-2017 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the mp++ library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <cstddef>
+#include <gmp.h>
+#include <random>
+#include <tuple>
+#include <type_traits>
+
+#include <mp++/mp++.hpp>
+
+#include "test_utils.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+static int ntries = 1000;
+
+using namespace mppp;
+using namespace mppp_test;
+
+using sizes = std::tuple<std::integral_constant<std::size_t, 1>, std::integral_constant<std::size_t, 2>,
+                         std::integral_constant<std::size_t, 3>, std::integral_constant<std::size_t, 6>,
+                         std::integral_constant<std::size_t, 10>>;
+
+static std::mt19937 rng;
+
+struct neg_tester {
+    template <typename S>
+    inline void operator()(const S &) const
+    {
+        using rational = rational<S::value>;
+        // Start with all zeroes.
+        mpq_raii m1, m2;
+        rational n1, n2;
+        ::mpq_neg(&m1.m_mpq, &m2.m_mpq);
+        neg(n1, n2);
+        REQUIRE((lex_cast(n1) == lex_cast(m1)));
+        // Test the other variants.
+        n1.neg();
+        REQUIRE((lex_cast(n1) == lex_cast(m1)));
+        REQUIRE((lex_cast(neg(n1)) == lex_cast(m1)));
+        mpq_raii tmp;
+        std::uniform_int_distribution<int> sdist(0, 1);
+        // Run a variety of tests with operands with x number of limbs.
+        auto random_xy = [&](unsigned x) {
+            for (int i = 0; i < ntries; ++i) {
+                if (sdist(rng) && sdist(rng) && sdist(rng)) {
+                    // Reset rop every once in a while.
+                    n1 = rational{};
+                }
+                random_rational(tmp, x, rng);
+                ::mpq_set(&m2.m_mpq, &tmp.m_mpq);
+                n2 = rational(&tmp.m_mpq);
+                if (sdist(rng)) {
+                    ::mpq_neg(&m2.m_mpq, &m2.m_mpq);
+                    n2.neg();
+                }
+                if (n2.get_num().is_static() && sdist(rng)) {
+                    // Promote sometimes, if possible.
+                    n2._get_num().promote();
+                }
+                if (n2.get_den().is_static() && sdist(rng)) {
+                    // Promote sometimes, if possible.
+                    n2._get_den().promote();
+                }
+                ::mpq_neg(&m1.m_mpq, &m2.m_mpq);
+                neg(n1, n2);
+                REQUIRE((lex_cast(n1) == lex_cast(m1)));
+                REQUIRE((lex_cast(n1) == lex_cast(neg(n2))));
+                n2.neg();
+                REQUIRE((lex_cast(n1) == lex_cast(n2)));
+            }
+        };
+
+        random_xy(0);
+        random_xy(1);
+        random_xy(2);
+        random_xy(3);
+        random_xy(4);
+    }
+};
+
+TEST_CASE("neg")
+{
+    tuple_for_each(sizes{}, neg_tester{});
+}
