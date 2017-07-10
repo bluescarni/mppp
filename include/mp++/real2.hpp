b0bc0a5d5a17d51d8d144f8684c374f759527d55
Francesco <bluescarni@gmail.com>
2017-07-10 17:01:31 +0200

    Various implementation bits in the new real class.

--- include/mp++/real2.hpp
+++ include/mp++/real2.hpp
@@ -18,7 +18,12 @@
 #include <cassert>
 #include <cstddef>
 #include <iostream>
+#include <limits>
 #include <mp++/detail/mpfr.hpp>
+#include <mp++/integer.hpp>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
 
 namespace mppp
 {
@@ -26,15 +31,94 @@ namespace mppp
 inline namespace detail
 {
 
-static_assert(MPFR_PREC_MIN > 0, "The minimum MPFR precision must be strictly positive.");
+// constexpr max/min implementations.
+template <typename T>
+constexpr T c_max(T a, T b)
+{
+    return a > b ? a : b;
+}
 
-// TODO further precision checks:
-// - min/max values must be able to be negated safely (use safe abs sint? and redefine the max if needed)
-//   basically, do something similar to the other real class. Keep also in mind the mpfr warnings
-//   about too large prec, probably we want to be conservative here.
-// - for a given SSize, the static storage must be enough to fit a min prec real (do a static
-//   assert in conjunction with the mpfr_custom_get_size() macro)
+template <typename T>
+constexpr T c_min(T a, T b)
+{
+    return a < b ? a : b;
+}
+
+// Compute a power of 2 value of the signed integer type T that can be safely negated.
+template <typename T>
+constexpr T safe_abs(T n = T(1))
+{
+    static_assert(std::is_integral<T>::value && std::is_signed<T>::value, "Invalid type.");
+    return (n <= std::numeric_limits<T>::max() / T(2) && n >= std::numeric_limits<T>::min() / T(2))
+               ? safe_abs(T(n * T(2)))
+               : n;
+}
+
+// Min prec that will be allowed for reals. It should be MPFR_PREC_MIN, as that value is currently 2.
+// We just put an extra constraint for paranoia, so that we ensure 100% that the min prec is not zero
+// (we rely on that for distinguishing static vs dynamic storage).
+constexpr ::mpfr_prec_t real_prec_min()
+{
+    return c_max(::mpfr_prec_t(1), ::mpfr_prec_t(MPFR_PREC_MIN));
+}
+
+// For the max precision, we first remove 7 bits from the MPFR_PREC_MAX value (as the MPFR docs warn
+// to never set the precision "close" to the max value). Then we make sure that we can negate safely.
+constexpr ::mpfr_prec_t real_prec_max()
+{
+    return c_min(::mpfr_prec_t(MPFR_PREC_MAX / 128), safe_abs<::mpfr_prec_t>());
+}
+
+// Sanity check.
+static_assert(real_prec_min() <= real_prec_max(),
+              "The minimum precision for real is larger than the maximum precision.");
+
 // TODO mpfr struct checks (see integer)
+
+// Helper function to print an mpfr to stream in a specified base.
+inline void mpfr_to_stream(const ::mpfr_t r, std::ostream &os, int base)
+{
+    // Check the base.
+    if (mppp_unlikely(base < 2 || base > 62)) {
+        throw std::invalid_argument("In the conversion of a real to string, a base of " + std::to_string(base)
+                                    + " was specified, but the only valid values are in the [2,62] range");
+    }
+
+    // Get the string fractional representation via the MPFR function,
+    // and wrap it into a smart pointer.
+    ::mpfr_exp_t exp(0);
+    smart_mpfr_str str(::mpfr_get_str(nullptr, &exp, 10, 0, r, MPFR_RNDN), ::mpfr_free_str);
+    if (mppp_unlikely(!str)) {
+        throw std::runtime_error("Error in the conversion of a real to string: the call to mpfr_get_str() failed");
+    }
+
+    // Print the string, inserting a decimal point after the first digit.
+    bool dot_added = false;
+    for (auto cptr = str.get(); *cptr != '\0'; ++cptr) {
+        os << (*cptr);
+        // NOTE: check this answer:
+        // http://stackoverflow.com/questions/13827180/char-ascii-relation
+        // """
+        // The mapping of integer values for characters does have one guarantee given
+        // by the Standard: the values of the decimal digits are continguous.
+        // (i.e., '1' - '0' == 1, ... '9' - '0' == 9)
+        // """
+        if (!dot_added && *cptr >= '0' && *cptr <= '9') {
+            os << '.';
+            dot_added = true;
+        }
+    }
+    assert(dot_added);
+
+    // Adjust the exponent. Do it in multiprec in order to avoid potential overflow.
+    integer<1> z_exp{exp};
+    --z_exp;
+    if (z_exp && !mpfr_zero_p(r)) {
+        // Add the exponent at the end of the string, if both the value and the exponent
+        // are nonzero.
+        os << "e" << z_exp;
+    }
+}
 
 template <std::size_t SSize>
 struct static_real {
@@ -46,6 +130,39 @@ struct static_real {
         // always be negated safely.
         return mpfr_struct_t{-_mpfr_prec, _mpfr_sign, _mpfr_exp, m_limbs.data()};
     }
+    mpfr_struct_t get_mpfr_c() const
+    {
+        // NOTE: as usual, we are using const_cast here to cast away the constness of the pointer.
+        // We need to make extra-sure the returned value is used only where const mpfr_t is expected.
+        return mpfr_struct_t{-_mpfr_prec, _mpfr_sign, _mpfr_exp, const_cast<::mp_limb_t *>(m_limbs.data())};
+    }
+    // Maximum precision possible for a static real.
+    static constexpr ::mpfr_prec_t max_prec_impl(::mpfr_prec_t n)
+    {
+        return n == std::numeric_limits<::mpfr_prec_t>::max()
+                   // Overflow check first.
+                   ? (throw std::overflow_error(
+                         "Overflow in the determination of the maximum precision for a static real."))
+                   : (mpfr_custom_get_size(n) <= SSize * sizeof(::mp_limb_t)
+                          // For the current precision n, we have enough storage. Try 1 bit more of precision.
+                          ? max_prec_impl(static_cast<::mpfr_prec_t>(n + 1))
+                          // We don't have enough storage for the current precision. Removing one bit should
+                          // give us a suitable precision.
+                          // NOTE: the min n possible here is real_prec_min(), and that is > 0. Hence, n - 1 is a safe
+                          // computation.
+                          : (mpfr_custom_get_size(static_cast<::mpfr_prec_t>(n - 1)) <= SSize * sizeof(::mp_limb_t)
+                                 ? static_cast<::mpfr_prec_t>(n - 1)
+                                 // This should never happen, as it would mean that 1 extra bit of precision
+                                 // requires 2 bytes of storage. Let's leave it for sanity check.
+                                 : (throw std::logic_error(
+                                       "Could not determine the maximum precision for a static real."))));
+    }
+    static constexpr ::mpfr_prec_t max_prec()
+    {
+        // NOTE: it's checked elsewhere that the value of real_prec_min() fits in
+        // static storage.
+        return max_prec_impl(real_prec_min());
+    }
     ::mpfr_prec_t _mpfr_prec;
     ::mpfr_sign_t _mpfr_sign;
     ::mpfr_exp_t _mpfr_exp;
@@ -56,7 +173,9 @@ template <std::size_t SSize>
 union real_union {
     using s_storage = static_real<SSize>;
     using d_storage = mpfr_struct_t;
-    real_union()
+    // Implementation of default ctor. This assumes that no member is currently active,
+    // and it will construct the static storage member with zero value and minimum precision.
+    void default_init()
     {
         // Activate the static member.
         ::new (static_cast<void *>(&m_st)) s_storage;
@@ -71,18 +190,24 @@ union real_union {
 
         // A temporary mpfr struct for use with the mpfr custom interface.
         mpfr_struct_t tmp;
-        // Init the limbs first, as indicated by the mpfr docs.
-        // TODO static assert enough storage.
-        mpfr_custom_init(m_st.m_limbs.data(), MPFR_PREC_MIN);
+        // Init the limbs first, as indicated by the mpfr docs. But first assert the static
+        // storage is enough to store a real with minimum precision.
+        static_assert(mpfr_custom_get_size(real_prec_min()) <= SSize * sizeof(::mp_limb_t),
+                      "Not enough storage in static_real to represent a real with minimum precision.");
+        mpfr_custom_init(m_st.m_limbs.data(), real_prec_min());
         // Do the custom init with a zero value, exponent 0 (unused), minimum precision (must match
         // the previous mpfr_custom_init() call), and the limbs array pointer.
-        mpfr_custom_init_set(&tmp, MPFR_ZERO_KIND, 0, MPFR_PREC_MIN, m_st.m_limbs.data());
+        mpfr_custom_init_set(&tmp, MPFR_ZERO_KIND, 0, real_prec_min(), m_st.m_limbs.data());
 
         // Copy over from tmp. The precision is set to the negative of the actual precision to signal static storage.
-        assert(tmp._mpfr_prec == MPFR_PREC_MIN);
-        m_st._mpfr_prec = -MPFR_PREC_MIN;
+        assert(tmp._mpfr_prec == real_prec_min());
+        m_st._mpfr_prec = -real_prec_min();
         m_st._mpfr_sign = tmp._mpfr_sign;
         m_st._mpfr_exp = tmp._mpfr_exp;
+    }
+    real_union()
+    {
+        default_init();
     }
     real_union(const real_union &r)
     {
@@ -96,8 +221,22 @@ union real_union {
             ::mpfr_init2(&m_dy, mpfr_get_prec(&r.g_dy()));
             ::mpfr_set(&m_dy, &r.g_dy(), MPFR_RNDN);
             // NOTE: use the function (rather than the macro) in order
-            // to avoid a warning about identical branches in recent GCC.
+            // to avoid a warning about identical branches in recent GCC versions.
             assert((mpfr_get_prec)(&m_dy) > 0);
+        }
+    }
+    real_union(real_union &&r) noexcept
+    {
+        if (r.is_static()) {
+            // Activate and init the static member with a copy.
+            ::new (static_cast<void *>(&m_st)) s_storage(r.g_st());
+        } else {
+            // Activate dynamic storage, and shallow copy r.
+            ::new (static_cast<void *>(&m_dy)) d_storage(r.g_dy());
+
+            // Deactivate the static storage in r, and default-init to static.
+            r.g_dy().~d_storage();
+            r.default_init();
         }
     }
     ~real_union()
@@ -160,6 +299,11 @@ public:
     {
         return m_real.is_dynamic();
     }
+    ::mpfr_prec_t get_prec() const
+    {
+        return static_cast<::mpfr_prec_t>(m_real.m_st._mpfr_prec > 0 ? m_real.m_st._mpfr_prec
+                                                                     : -m_real.m_st._mpfr_prec);
+    }
     const real_union<SSize> &_get_union() const
     {
         return m_real;
@@ -177,9 +321,12 @@ template <std::size_t SSize>
 inline std::ostream &operator<<(std::ostream &os, const real2<SSize> &r)
 {
     if (r.is_static()) {
-
+        const auto m = r._get_union().g_st().get_mpfr_c();
+        mpfr_to_stream(&m, os, 10);
     } else {
+        mpfr_to_stream(&r._get_union().g_dy(), os, 10);
     }
+    return os;
 }
 }
 
--- test/real2_basic.cpp
+++ test/real2_basic.cpp
@@ -16,4 +16,17 @@ using namespace mppp;
 TEST_CASE("real constructors")
 {
     real2<1> r, r2{r};
+    std::cout << real_prec_min() << '\n';
+    std::cout << real_prec_max() << '\n';
+    std::cout << r << '\n';
+    std::cout << r.get_prec() << '\n';
+    std::cout << sizeof(r) << '\n';
+    std::cout << sizeof(mpfr_struct_t) << '\n';
+    std::cout << static_real<1>::max_prec() << '\n';
+    std::cout << static_real<2>::max_prec() << '\n';
+    std::cout << static_real<3>::max_prec() << '\n';
+    std::cout << static_real<4>::max_prec() << '\n';
+    std::cout << static_real<5>::max_prec() << '\n';
+    std::cout << static_real<6>::max_prec() << '\n';
+    constexpr auto foo = static_real<1>::max_prec();
 }
