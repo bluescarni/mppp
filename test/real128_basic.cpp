1babc7e8e0fab15592b3a89991f712a095bb587e
Francesco Biscani <bluescarni@gmail.com>
2017-08-15 01:24:58 +0200

    Some initial work on testing.

--- test/real128_basic.cpp
+++ test/real128_basic.cpp
@@ -6,54 +6,60 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include <mp++/real128.hpp>
+#include <utility>
+
+#include <mp++/mp++.hpp>
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
 using namespace mppp;
 
+using int_t = integer<1>;
+
 TEST_CASE("real128 constructors")
 {
-    __float128 x = str_to_float128("1.1");
-    float128_stream(std::cout, x);
-    constexpr real128 r1{1.l}, r2{2}, r3{__float128{1}};
-    real128 blap{integer<1>{
-        "1231238012938120938201938029138901283029138012983091283091283091283091201310012301301029381318240752987"}};
-    float128_stream(std::cout, blap.m_value);
-    std::cout << "\n";
-    real128 blap2{integer<1>{1} << 120};
-    float128_stream(std::cout, blap2.m_value);
-    std::cout << "\n";
-    real128 blap3{integer<1>{1} << 50};
-    float128_stream(std::cout, blap3.m_value);
-    std::cout << "\n";
-    real128 blap4{integer<1>{1} << 500};
-    float128_stream(std::cout, blap4.m_value);
-    std::cout << "\n";
-    real128 blap5{rational<1>{"3232338832138213/-43834982310"}};
-    float128_stream(std::cout, blap5.m_value);
-    std::cout << "\n";
-    std::cout << real128{"1.1"} << '\n';
-    std::cout << real128{1.1} << '\n';
-    const char *mystr = "1.1";
-    std::cout << static_cast<double>(real128{mystr, mystr + 3}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{mystr, mystr + 3}) << '\n';
-    std::cout << real128{integer<1>{"1231238012938120938201938029138901283029138012983091283091"
-                                    "283091283091201310012301301029381318240752987"}}
-              << '\n';
-    std::cout << static_cast<integer<1>>(real128{integer<1>{"1231238012938120938201938029138901283029138012983091283091"
-                                                            "283091283091201310012301301029381318240752987"}})
-              << '\n';
-    std::cout << static_cast<integer<1>>(real128{1}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{0.9}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{1.1}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{12.1}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{-12.1}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{-123.1}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{"1E-4964"}) << '\n';
-    std::cout << static_cast<integer<1>>(real128{"-1E-4964"}) << '\n';
-    std::cout << real128{"-1E-4964"} << '\n';
-    std::cout << static_cast<rational<1>>(real128{"-1E-4964"}) << '\n';
-    std::cout << real128{static_cast<rational<1>>(real128{"-1E-4964"})} << '\n';
+    real128 r;
+    REQUIRE((r.m_value == 0));
+    r.m_value = 12;
+    real128 r2{r};
+    REQUIRE((r2.m_value == 12));
+    real128 r3{std::move(r)};
+    REQUIRE((r3.m_value == 12));
+    REQUIRE((r.m_value == 12));
+    real128 r4{::__float128(-56)};
+    REQUIRE((r4.m_value == -56));
+    real128 r5{-123};
+    REQUIRE((r5.m_value == -123));
+    real128 r6{124ull};
+    REQUIRE((r6.m_value == 124));
+    real128 r7{-0.5};
+    REQUIRE((r7.m_value == -0.5));
+    real128 r8{1.5f};
+    REQUIRE((r8.m_value == 1.5f));
+#if defined(MPPP_WITH_MPFR)
+    real128 r8a{1.5l};
+    REQUIRE((r8a.m_value == 1.5l));
+#endif
+    // Construction from integer.
+    REQUIRE((real128{int_t{0}}.m_value == 0));
+    int_t n{123};
+    REQUIRE((real128{n}.m_value == 123));
+    n = -123;
+    n.promote();
+    REQUIRE((real128{n}.m_value == -123));
+    // Use a couple of limbs, nbits does not divide GMP_NUMB_BITS exactly.
+    n = -1;
+    n <<= GMP_NUMB_BITS + 1;
+    REQUIRE((real128{n}.m_value == ::scalbnq(::__float128(-1), GMP_NUMB_BITS + 1)));
+    n.promote();
+    n.neg();
+    REQUIRE((real128{n}.m_value == ::scalbnq(::__float128(1), GMP_NUMB_BITS + 1)));
+    // Use two limbs, nbits dividing exactly.
+    n = -2;
+    n <<= 2 * GMP_NUMB_BITS - 1;
+    REQUIRE((real128{n}.m_value == ::scalbnq(::__float128(-2), 2 * GMP_NUMB_BITS - 1)));
+    n.promote();
+    n.neg();
+    REQUIRE((real128{n}.m_value == ::scalbnq(::__float128(2), 2 * GMP_NUMB_BITS - 1)));
 }
