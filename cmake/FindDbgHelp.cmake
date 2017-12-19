9e966f62aef926dd11787c06a104189bb84268f9
Francesco Biscani <bluescarni@gmail.com>
2017-12-19 22:53:02 +0100

    Alternative FindDbgHelp.cmake.

--- cmake/FindDbgHelp.cmake
+++ cmake/FindDbgHelp.cmake
@@ -1,51 +1,22 @@
-# Slightly modified version from:
-# https://github.com/openclonk/openclonk/blob/master/cmake/FindDbgHelp.cmake
-# Original copyright notice follows.
+# Locate the DbgHelp library for MSVC.
 
-# OpenClonk, http://www.openclonk.org
-#
-# Copyright (c) 2011-2017, The OpenClonk Team and contributors
-#
-# Distributed under the terms of the ISC license; see accompanying file
-# "COPYING" for details.
-#
-# "Clonk" is a registered trademark of Matthes Bender, used with permission.
-# See accompanying file "TRADEMARK" for details.
-#
-# To redistribute this file separately, substitute the full license texts
-# for the above references.
-
-# - Find DbgHelp
-# Find the DbgHelp library
-# This module defines
-#  DBGHELP_INCLUDE_DIR, where to find dbghelp.h, etc.
-#  DBGHELP_LIBRARIES, the libraries needed to use DbgHelp.
-#  DBGHELP_FOUND, If false, do not try to use DbgHelp.
-
-# MSVC knows how to find the Windows SDK on its own, and replicating that
-# in CMake is very tricky, especially with the SDK's layout changing
-# occasionally. So instead we'll just leave it to MSVC to do correctly.
-if(MSVC AND NOT DEFINED DBGHELP_INCLUDE_DIR AND NOT DEFINED DBGHELP_LIBRARY)
-	set(DBGHELP_INCLUDE_DIR "")
-	set(DBGHELP_LIBRARY "dbghelp")
-	set(DBGHELP_FOUND TRUE)
+if(DbgHelp_INCLUDE_DIR AND DbgHelp_LIBRARY)
+    # Already in cache, be silent
+    set(DbgHelp_FIND_QUIETLY TRUE)
 endif()
 
-if(NOT DBGHELP_FOUND)
-	find_path(DBGHELP_INCLUDE_DIR NAMES dbghelp.h PATH_SUFFIXES include)
-	set(DBGHELP_NAMES ${DBGHELP_NAMES} dbghelp)
-	find_library(DBGHELP_LIBRARY NAMES ${DBGHELP_NAMES})
+find_path(DbgHelp_INCLUDE_DIR NAMES Dbghelp.h)
+find_library(DbgHelp_LIBRARY NAMES dbghelp)
 
-	include(FindPackageHandleStandardArgs)
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(DBGHELP DEFAULT_MSG DBGHELP_LIBRARY DBGHELP_INCLUDE_DIR)
-endif()
+include(FindPackageHandleStandardArgs)
 
-if(DBGHELP_FOUND)
-    set(DBGHELP_LIBRARIES ${DBGHELP_LIBRARY})
+find_package_handle_standard_args(DbgHelp DEFAULT_MSG DbgHelp_INCLUDE_DIR DbgHelp_LIBRARY)
+
+mark_as_advanced(DbgHelp_INCLUDE_DIR DbgHelp_LIBRARY)
+
+# NOTE: this has been adapted from CMake's FindPNG.cmake.
+if(DbgHelp_FOUND AND NOT TARGET DbgHelp::DbgHelp)
     add_library(DbgHelp::DbgHelp UNKNOWN IMPORTED)
-    set_target_properties(DbgHelp::DbgHelp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${DBGHELP_INCLUDE_DIR}"
-        IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-        IMPORTED_LOCATION "${DBGHELP_LIBRARIES}")
+    set_target_properties(DbgHelp::DbgHelp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${DbgHelp_INCLUDE_DIR}"
+        IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${DbgHelp_LIBRARY}")
 endif()
-
-mark_as_advanced(DBGHELP_LIBRARY DBGHELP_INCLUDE_DIR)
