36e7b95a36a25dffe8f465c26800aaa27133fbc7
Francesco Biscani <bluescarni@gmail.com>
2018-02-12 11:55:58 +0100

    For sphinx<1.7 in the CI pipeline for now.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -101,7 +101,7 @@ elif [[ "${MPPP_BUILD}" == "Documentation" ]]; then
         exit 1;
     fi
     cd ../sphinx;
-    pip install --user sphinx breathe requests[security] sphinx-bootstrap-theme
+    pip install --user "sphinx<1.7" breathe requests[security] sphinx-bootstrap-theme
     export SPHINX_OUTPUT=`make html linkcheck 2>&1 >/dev/null`;
     if [[ "${SPHINX_OUTPUT}" != "" ]]; then
         echo "Sphinx encountered some problem:";
