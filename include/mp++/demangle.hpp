02f10d0d4ad5462f71ff53536143814dc5fdc518
Francesco Biscani <bluescarni@gmail.com>
2019-04-10 00:22:10 +0200

    Some initial code for the library.

---
+++ include/mp++/demangle.hpp
@@ -0,0 +1,17 @@
+// Copyright 2016-2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the mp++ library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef MPPP_DEMANGLE_HPP
+#define MPPP_DEMANGLE_HPP
+
+namespace piranha
+{
+
+}
+
+#endif
