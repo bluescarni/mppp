f60aa63f24c7ec1db31c5606580a32aa7207aab1
Francesco Biscani <bluescarni@gmail.com>
2019-04-19 00:24:55 +0200

    Attempt switch to clang. [skip ci]

--- tools/circleci_cosmic_gcc8_debuggmp_unstable.sh
+++ tools/circleci_cosmic_gcc8_debuggmp_unstable.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake mercurial autoconf bison texinfo
+sudo apt-get install build-essential cmake mercurial autoconf bison texinfo clang
 
 # Create the build dir and cd into it.
 mkdir build
@@ -17,13 +17,13 @@ cd build
 hg clone https://gmplib.org/repo/gmp/ gmp_unstable
 cd gmp_unstable
 ./.bootstrap
-./configure --enable-shared --disable-static --enable-assert --enable-alloca=debug --disable-assembly CFLAGS="-g -fsanitize=address" --prefix=/home/circleci/.local
+CC=clang CXX=clang++ ./configure --enable-shared --disable-static --enable-assert --enable-alloca=debug --disable-assembly CFLAGS="-g -fsanitize=address" --prefix=/home/circleci/.local
 make -j2
 make install
 
 # Compile mppp and run the tests.
 cd ..
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DCMAKE_PREFIX_PATH=/home/circleci/.local
+CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address" -DCMAKE_PREFIX_PATH=/home/circleci/.local
 make -j2 VERBOSE=1
 ctest -V
 
