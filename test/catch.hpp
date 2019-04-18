923d5a9740e57386161eec0c47e120f03de203aa
Francesco Biscani <bluescarni@gmail.com>
2019-04-19 00:07:51 +0200

    Try to tweak the stack size in catch.

--- test/catch.hpp
+++ test/catch.hpp
@@ -8161,7 +8161,7 @@ namespace Catch {
 
     // 32kb for the alternate stack seems to be sufficient. However, this value
     // is experimentally determined, so that's not guaranteed.
-    constexpr static std::size_t sigStackSize = 32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ;
+    constexpr static std::size_t sigStackSize = (32768 >= MINSIGSTKSZ ? 32768 : MINSIGSTKSZ) * 10;
 
     static SignalDefs signalDefs[] = {
         { SIGINT,  "SIGINT - Terminal interrupt signal" },
