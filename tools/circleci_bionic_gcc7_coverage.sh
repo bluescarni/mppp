fd45d8f71c1ccab6f1cf01a7e0d15c423a4a0d55
Francesco Biscani <bluescarni@gmail.com>
2019-04-19 15:37:27 +0200

    Disable the pybind build on bionic (pybind version is too old).

--- tools/circleci_bionic_gcc7_coverage.sh
+++ tools/circleci_bionic_gcc7_coverage.sh
@@ -7,14 +7,14 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev curl python-pybind11 python-dev
+sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev curl
 
 # Create the build dir and cd into it.
 mkdir build
 cd build
 
 # GCC build.
-cmake ../ -DCMAKE_CXX_STANDARD=14 -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=YES -DMPPP_WITH_MPFR=yes -DMPPP_WITH_QUADMATH=yes -DCMAKE_CXX_FLAGS="--coverage -fconcepts -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" -DMPPP_TEST_PYBIND11=yes -DPYBIND11_PYTHON_VERSION=2.7
+cmake ../ -DCMAKE_CXX_STANDARD=14 -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=YES -DMPPP_WITH_MPFR=yes -DMPPP_WITH_QUADMATH=yes -DCMAKE_CXX_FLAGS="--coverage -fconcepts -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC"
 make -j2 VERBOSE=1
 # Run the tests.
 ctest -V
