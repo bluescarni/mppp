917b0eb3cdf5aabfece651c3bc51a51fd30b2065
Francesco Biscani <bluescarni@gmail.com>
2019-06-14 17:10:31 +0200

    Add a relwithdebinfo build on circleci.

---
+++ tools/circleci_bionic_gcc7_relwithdebinfo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# GCC build.
+cmake ../ -DCMAKE_CXX_STANDARD=17 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DMPPP_BUILD_TESTS=YES -DMPPP_WITH_MPFR=yes -DMPPP_WITH_QUADMATH=yes -DCMAKE_CXX_FLAGS="-fconcepts"
+make -j2 VERBOSE=1
+# Run the tests.
+ctest -V
+
+set +e
+set +x
