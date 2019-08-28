f3b8b00eaf3d60c34249c5b54cf2fe04d3e23bf9
Francesco Biscani <bluescarni@gmail.com>
2019-08-28 20:19:31 +0200

    Internal docs.

--- test/catch_main.cpp
+++ test/catch_main.cpp
@@ -10,5 +10,13 @@
 // https://github.com/catchorg/Catch2/blob/master/docs/slow-compiles.md
 
 #define CATCH_CONFIG_MAIN
+
+// NOTE: the unicode MSVC builds lead to catch
+// defining a wmain() function rather than the usual
+// main, and this leads to undefined references
+// in the appveyor builds. Luckily, there is
+// this definition one can set to use the good
+// ole main() instead.
 #define DO_NOT_USE_WMAIN
+
 #include "catch.hpp"
