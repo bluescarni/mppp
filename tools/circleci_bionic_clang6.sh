a7712d11a9edf4b344897fcf5362769f03f97f14
Francesco Biscani <bluescarni@gmail.com>
2020-03-16 20:34:47 +0100

    Ditto.

--- tools/circleci_bionic_clang6.sh
+++ tools/circleci_bionic_clang6.sh
@@ -7,18 +7,18 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake clang libgmp-dev libmpfr-dev gcc-8
+sudo apt-get install build-essential cmake clang libgmp-dev libmpfr-dev
 
 # Create the build dir and cd into it.
 mkdir build
 cd build
 
 # Compile mppp and run the tests.
-CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_WITH_MPFR=yes -DMPPP_BUILD_TESTS=yes -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/8/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/8/libquadmath.so
+CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_WITH_MPFR=yes -DMPPP_BUILD_TESTS=yes -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/7/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/7/libquadmath.so
 make -j2 VERBOSE=1
 ctest -V
 
-CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_WITH_MPFR=no -DMPPP_BUILD_TESTS=yes -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/8/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/8/libquadmath.so
+CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_WITH_MPFR=no -DMPPP_BUILD_TESTS=yes -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/7/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/7/libquadmath.so
 make -j2 VERBOSE=1
 ctest -V
 
