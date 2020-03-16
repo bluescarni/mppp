9e4d5a7e6c30b9950c44844f44ed2fa8bf4b6137
Francesco Biscani <bluescarni@gmail.com>
2020-03-16 20:28:49 +0100

    Fixes.

--- tools/circleci_xenial_clang6.sh
+++ tools/circleci_xenial_clang6.sh
@@ -7,14 +7,13 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake clang libgmp-dev libmpfr-dev gcc-5
+sudo apt-get install build-essential cmake clang-6 libgmp-dev libmpfr-dev gcc-5
 
 # Create the build dir and cd into it.
 mkdir build
 cd build
 
 # Compile mppp and run the tests.
-cd ..
 CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DMPPP_WITH_QUADMATH=yes -DMPPP_WITH_MPFR=yes -DMPPP_BUILD_TESTS=yes -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/5/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/5/libquadmath.so
 make -j2 VERBOSE=1
 ctest -V
