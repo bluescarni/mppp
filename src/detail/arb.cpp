d1720168d2f7df0b9cfc4c0f1bcea785611bd5ed
Francesco Biscani <bluescarni@gmail.com>
2020-06-10 10:48:21 +0200

    Try to quench some warnings in the flint/Arb code on MSVC.

--- src/detail/arb.cpp
+++ src/detail/arb.cpp
@@ -10,6 +10,16 @@
 
 #include <stdexcept>
 
+#if defined(_MSC_VER) && !defined(__clang__)
+
+// Disable some warnings for MSVC.
+#pragma warning(push)
+#pragma warning(disable : 4146)
+#pragma warning(disable : 4244)
+#pragma warning(disable : 4267)
+
+#endif
+
 #include <flint/flint.h>
 #include <flint/fmpz.h>
 
@@ -21,6 +31,12 @@
 #if defined(MPPP_WITH_MPC)
 
 #include <acb.h>
+
+#endif
+
+#if defined(_MSC_VER) && !defined(__clang__)
+
+#pragma warning(pop)
 
 #endif
 
