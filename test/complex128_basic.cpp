90736e8934077ad77ab44b981ee702c6c49d29a0
Francesco Biscani <bluescarni@gmail.com>
2021-04-01 17:34:54 +0200

    Test debug.

--- test/complex128_basic.cpp
+++ test/complex128_basic.cpp
@@ -180,6 +180,7 @@ TEST_CASE("basic constructors")
     REQUIRE(c12.m_value == cplex128{1, 2});
 #else
     REQUIRE(!std::is_constructible<complex128, std::complex<long double>>::value);
+    complex128 foobar(std::complex<long double>{});
 #endif
 }
 
