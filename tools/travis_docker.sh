5a0912ce67a19b06a704b495e53febed247c4913
Francesco Biscani <bluescarni@gmail.com>
2021-09-01 22:17:03 +0200

    Simplifications.

--- tools/travis_docker.sh
+++ tools/travis_docker.sh
@@ -6,8 +6,7 @@ set -x
 # Exit on error.
 set -e
 
-docker pull ubuntu:focal
-docker run --rm -e TRAVIS_TAG -v `pwd`:/mppp ubuntu:focal bash /mppp/tools/travis_ubuntu_ppc64.sh
+docker run --rm -v `pwd`:/mppp ubuntu:focal bash /mppp/tools/travis_ubuntu_ppc64.sh
 
 set +e
 set +x
