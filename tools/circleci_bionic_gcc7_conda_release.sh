05509b4d5961ff531fb2843a87400e3a3e038426
Francesco Biscani <bluescarni@gmail.com>
2022-05-10 11:18:27 +0200

    Move various static checks in a .cpp file, workaround for what looks like a GCC bug in the tests.

--- tools/circleci_bionic_gcc7_conda_release.sh
+++ tools/circleci_bionic_gcc7_conda_release.sh
@@ -29,7 +29,7 @@ cd build
 cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_CXX_STANDARD=17 -DCMAKE_BUILD_TYPE=Release -DMPPP_BUILD_TESTS=YES -DBoost_NO_BOOST_CMAKE=ON -DMPPP_WITH_BOOST_S11N=yes -DMPPP_WITH_MPFR=yes -DMPPP_WITH_MPC=yes -DMPPP_WITH_ARB=yes -DMPPP_WITH_QUADMATH=yes -DCMAKE_CXX_FLAGS="-fconcepts" -DMPPP_ENABLE_IPO=yes -DMPPP_BUILD_BENCHMARKS=yes
 make -j2 VERBOSE=1
 # Run the tests.
-ctest -j2 -E integer_hash -V
+ctest -j2 -V
 
 # Run the benchmarks as well.
 make -j2 VERBOSE=1 benchmark
