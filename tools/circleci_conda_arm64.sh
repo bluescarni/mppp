85982c9b6d9f5ae28a4188430d828567d6515be8
Francesco Biscani <bluescarni@gmail.com>
2023-11-23 11:43:57 +0100

    More circleCI WIP.

--- tools/circleci_conda_arm64.sh
+++ tools/circleci_conda_arm64.sh
@@ -7,14 +7,14 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install wget curl
+sudo apt-get install build-essential wget
 
 # Install conda+deps.
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O mambaforge.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh -O mambaforge.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/mambaforge/bin:$PATH"
 bash mambaforge.sh -b -p $HOME/mambaforge
-mamba create -y -p $deps_dir c-compiler cxx-compiler ninja cmake gmp mpfr 'libflint<3' arb mpc fmt libboost-devel lcov
+mamba create -y -p $deps_dir c-compiler cxx-compiler ninja cmake gmp mpfr 'libflint<3' arb mpc fmt libboost-devel
 source activate $deps_dir
 
 # Create the build dir and cd into it.
@@ -28,24 +28,16 @@ cmake ../ -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DMPPP_BUILD_TESTS=yes \
     -DBoost_NO_BOOST_CMAKE=yes \
+    -DMPPP_WITH_BOOST_S11N=yes \
+    -DMPPP_WITH_FMT=yes \
     -DMPPP_WITH_MPFR=yes \
     -DMPPP_WITH_MPC=yes \
     -DMPPP_WITH_ARB=yes \
-    -DMPPP_WITH_QUADMATH=yes \
-    -DMPPP_WITH_BOOST_S11N=yes \
-    -DCMAKE_CXX_FLAGS="--coverage"
-    
+    -DMPPP_ENABLE_IPO=yes
+
 ninja -v -j4
 # Run the tests.
 ctest -V -j4
-
-# Create lcov report
-lcov --capture --directory . --output-file coverage.info
-
-# Upload coverage data.
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-chmod +x codecov
-./codecov -f coverage.info -g --gx $deps_dir/bin/gcov
 
 set +e
 set +x
