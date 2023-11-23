c46fa84b168ff8c61d191b6c70d82e36aa485763
Francesco Biscani <bluescarni@gmail.com>
2023-11-23 11:57:35 +0100

    First OSX build attempt.

--- tools/gha_osx.sh
+++ tools/gha_osx.sh
@@ -8,22 +8,29 @@ set -x
 set -e
 
 # Install conda+deps.
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh -O miniforge.sh
 export deps_dir=$HOME/local
-export PATH="$HOME/miniconda/bin:$PATH"
-bash miniconda.sh -b -p $HOME/miniconda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda create -y -q -p $deps_dir c-compiler cxx-compiler libcxx cmake gmp mpfr arb libflint mpc boost-cpp fmt
+export PATH="$HOME/miniforge/bin:$PATH"
+bash miniforge.sh -b -p $HOME/miniforge
+mamba create -y -p $deps_dir c-compiler cxx-compiler ninja cmake gmp mpfr 'libflint<3' arb mpc fmt libboost-devel
 source activate $deps_dir
 
 # Create the build dir and cd into it.
 mkdir build
 cd build
 
-# GCC build.
-CXX=clang++ CC=clang cmake -DCMAKE_BUILD_TYPE=Debug -DMPPP_BUILD_TESTS=yes -DBoost_NO_BOOST_CMAKE=ON -DMPPP_WITH_BOOST_S11N=yes -DMPPP_WITH_FMT=yes -DMPPP_WITH_MPFR=yes -DMPPP_WITH_ARB=yes -DMPPP_WITH_MPC=yes -DMPPP_ENABLE_IPO=yes ../
-make -j2 VERBOSE=1
+cmake ../ -G Ninja \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DMPPP_BUILD_TESTS=yes \
+    -DBoost_NO_BOOST_CMAKE=yes \
+    -DMPPP_WITH_BOOST_S11N=yes \
+    -DMPPP_WITH_FMT=yes \
+    -DMPPP_WITH_MPFR=yes \
+    -DMPPP_WITH_ARB=yes \
+    -DMPPP_WITH_MPC=yes \
+    -DMPPP_ENABLE_IPO=yes
+
+ninja -v
 ctest -V -j2
 
 set +e
